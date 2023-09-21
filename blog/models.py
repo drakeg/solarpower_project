14190e99c87cd0d3313c027782a92d88acf7119a
Greg Drake <greg@madmallards.com>
2023-09-21 00:52:37 -0400

    Updated blog model.

--- blog/models.py
+++ blog/models.py
@@ -5,5 +5,10 @@ from django.contrib.auth.models import User
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='blog_images/', null=True, blank=True)
+    keywords = models.CharField(max_length=200, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_published = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
