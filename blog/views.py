5c9975cf0f6a94df66c33cfc1eed96e9d4ead5f7
Greg Drake <greg@madmallards.com>
2023-09-21 01:01:08 -0400

    More blog updates.

--- blog/views.py
+++ blog/views.py
@@ -6,11 +6,11 @@ from django.contrib.auth.decorators import login_required
 
 def blog_list(request):
     blog_posts = BlogPost.objects.all().order_by('-date_published')
-    return render(request, 'solarpower/blog/blog_list.html', {'blog_posts': blog_posts})
+    return render(request, 'blog/blog_list.html', {'blog_posts': blog_posts})
 
 def blog_detail(request, pk):
     blog_post = get_object_or_404(BlogPost, pk=pk)
-    return render(request, 'solarpower/blog/blog_detail.html', {'blog_post': blog_post})
+    return render(request, 'blog/blog_detail.html', {'blog_post': blog_post})
 
 @login_required
 def create_blog_post(request):
@@ -20,7 +20,7 @@ def create_blog_post(request):
             blog_post = form.save(commit=False)
             blog_post.author = request.user
             blog_post.save()
-            return redirect('solarpower:blog_detail', pk=blog_post.pk)
+            return redirect('blog:blog_detail', pk=blog_post.pk)
     else:
         form = BlogPostForm()
-    return render(request, 'solarpower/blog/blog_post_form.html', {'form': form})
+    return render(request, 'blog/blog_post_form.html', {'form': form})
