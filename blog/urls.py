c54b888275096c3203ee343b14a67b562cfab195
Greg Drake <greg@madmallards.com>
2023-09-21 00:58:30 -0400

    More blog updates.

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.blog_list, name='blog_list'),
+    path('<int:pk>/', views.blog_detail, name='blog_detail'),
+    path('new/', views.create_blog_post, name='create_blog_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+# solarpower/views.py
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import BlogPost
+from .forms import BlogPostForm  # We'll create this form shortly
+from django.contrib.auth.decorators import login_required
 
-def post_list(request):
-    post = BlogPost.objects.all()
-    return render(request, 'blog/post_list.html', {'post': post})
+def blog_list(request):
+    blog_posts = BlogPost.objects.all().order_by('-date_published')
+    return render(request, 'solarpower/blog/blog_list.html', {'blog_posts': blog_posts})
+
+def blog_detail(request, pk):
+    blog_post = get_object_or_404(BlogPost, pk=pk)
+    return render(request, 'solarpower/blog/blog_detail.html', {'blog_post': blog_post})
+
+@login_required
+def create_blog_post(request):
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            blog_post = form.save(commit=False)
+            blog_post.author = request.user
+            blog_post.save()
+            return redirect('solarpower:blog_detail', pk=blog_post.pk)
+    else:
+        form = BlogPostForm()
+    return render(request, 'solarpower/blog/blog_post_form.html', {'form': form})
