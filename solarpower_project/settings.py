16aee12da6621e6d3a947ccb1861dae9694c274f
Greg Drake <greg@madmallards.com>
2023-10-02 15:11:23 -0400

    Added LOGOUT_URL

--- solarpower_project/settings.py
+++ solarpower_project/settings.py
@@ -135,6 +135,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login/'
+LOGOUT_URL = '/logout/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
