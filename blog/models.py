f4d6e18bee93db9e72d5c8fd902d4ec0b22cbb20
Greg Drake <greg@madmallards.com>
2026-09-23 19:32:07 -0400

    fix: resolve blocking Django lint issues

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,14 @@
-# models.py in the solarpower app
+# models.py in the blog app
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
+
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     image = models.ImageField(upload_to='blog_images/', null=True, blank=True)
     keywords = models.CharField(max_length=200, blank=True, null=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_published = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
