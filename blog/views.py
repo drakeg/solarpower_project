83a8900b7f62e06c462bcc0ae4410c936f66b1af
Greg Drake <greg@madmallards.com>
2026-09-23 19:33:17 -0400

    lint: clean legacy Django warnings

--- blog/views.py
+++ blog/views.py
@@ -1,39 +1,46 @@
-# solarpower/views.py
+# blog/views.py
 import nltk
-from nltk.tokenize import sent_tokenize
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import BlogPost
-from .forms import BlogPostForm  # We'll create this form shortly
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, redirect, render
+from nltk.tokenize import sent_tokenize
 
-nltk.download('punkt')  # Download the NLTK data for tokenization
+from .forms import BlogPostForm
+from .models import BlogPost
+
+nltk.download('punkt')
+
 
 def generate_summary(article_text, sentences_count):
     sentences = sent_tokenize(article_text)
+    return ' '.join(sentences[:sentences_count])
 
-    first_two_sentences = ' '.join(sentences[:sentences_count])
-    return first_two_sentences
 
 def home(request):
-    # Query and customize the blog posts you want to display
     active_page = 'blog'
-    blog_posts = BlogPost.objects.all().order_by('-date_published') # Example: Display the latest 5 blog posts
+    blog_posts = BlogPost.objects.all().order_by('-date_published')
     paginator = Paginator(blog_posts, 10)
     for post in blog_posts:
         post.summary = generate_summary(post.content, sentences_count=2)
-        if post.keywords:
-            post.keywords_list = post.keywords.split(', ')  # Split keywords into a list
-        else:
-            post.keywords_list = []  # Handle cases where no keywords are provided
+        post.keywords_list = post.keywords.split(', ') if post.keywords else []
     page = request.GET.get('page')
     blog_posts = paginator.get_page(page)
-    return render(request, 'blog/blog_list.html', {'blog_posts': blog_posts, 'active_page': active_page})
+    return render(
+        request,
+        'blog/blog_list.html',
+        {'blog_posts': blog_posts, 'active_page': active_page},
+    )
+
 
 def blog_detail(request, pk):
     active_page = 'blog'
     blog_post = get_object_or_404(BlogPost, pk=pk)
-    return render(request, 'blog/blog_detail.html', {'blog_post': blog_post, 'active_page': active_page})
+    return render(
+        request,
+        'blog/blog_detail.html',
+        {'blog_post': blog_post, 'active_page': active_page},
+    )
+
 
 @login_required
 def create_blog_post(request):
@@ -47,4 +54,8 @@ def create_blog_post(request):
             return redirect('blog:blog_detail', pk=blog_post.pk)
     else:
         form = BlogPostForm()
-    return render(request, 'blog/blog_post_form.html', {'form': form, 'active_page': active_page})
+    return render(
+        request,
+        'blog/blog_post_form.html',
+        {'form': form, 'active_page': active_page},
+    )
