c4d73074e0468516e25eb7d7238d44084fa59503
Greg Drake <greg@madmallards.com>
2026-09-23 19:32:04 -0400

    fix: resolve blocking Django lint issues

--- solarpower/views.py
+++ solarpower/views.py
@@ -1,26 +1,27 @@
 # views.py in the solarpower app
-from django.shortcuts import render, redirect
-from .models import *
 from django.contrib import messages
-from django.contrib.auth import logout
-from django.contrib.auth import login
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LogoutView
+from django.shortcuts import redirect, render
+
 
 logout_view = LogoutView.as_view()
+
 
 def home(request):
     active_page = 'home'
     return render(request, 'home.html', {'active_page': active_page})
 
+
 def calculator(request):
     active_page = 'calculator'
-    # Add any necessary context data for your calculator page here
     return render(request, 'calculator.html', {'active_page': active_page})
 
+
 def user_profile(request):
-    # Add any necessary context data for the user profile page here
     return render(request, 'user_profile.html', {'user': request.user})
+
 
 def register(request):
     if request.method == 'POST':
@@ -33,7 +34,8 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
 
+
 def custom_logout(request):
     logout(request)
     messages.success(request, "You have been logged out successfully.")
-    return redirect('home')  # Redirect to the home page
+    return redirect('home')
