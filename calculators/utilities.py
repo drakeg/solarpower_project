e5943f82fefbb3889612fd3a5d2c3324b9c639a1
Greg Drake <greg@madmallards.com>
2026-09-25 00:32:52 -0400

    feature: calculate solar array and battery sizing

--- calculators/utilities.py
+++ calculators/utilities.py
@@ -19,3 +19,26 @@ def calculate_savings(input_data):
         'lifetime_utility_cost': lifetime_utility_cost,
         'lifetime_net_savings': lifetime_net_savings,
     }
+
+
+def calculate_system_size(input_data):
+    daily_energy_use = input_data['daily_energy_use']
+    peak_sun_hours = input_data['peak_sun_hours']
+    efficiency = input_data['system_efficiency'] / 100
+    autonomy_days = input_data['autonomy_days']
+    battery_voltage = input_data['battery_voltage']
+    usable_battery_fraction = input_data['usable_battery_percent'] / 100
+
+    required_array_watts = daily_energy_use / peak_sun_hours / efficiency
+    required_usable_battery_wh = daily_energy_use * autonomy_days
+    required_nominal_battery_wh = (
+        required_usable_battery_wh / usable_battery_fraction
+    )
+    required_battery_ah = required_nominal_battery_wh / battery_voltage
+
+    return {
+        'required_array_watts': required_array_watts,
+        'required_usable_battery_wh': required_usable_battery_wh,
+        'required_nominal_battery_wh': required_nominal_battery_wh,
+        'required_battery_ah': required_battery_ah,
+    }
