5a32401129e588ad932c888abf721c9a5d3bfa99
Greg Drake <greg@madmallards.com>
2026-09-25 00:33:04 -0400

    feature: route solar system sizing calculator

--- calculators/urls.py
+++ calculators/urls.py
@@ -5,4 +5,5 @@ app_name = 'calculators'
 
 urlpatterns = [
     path('solar-savings/', views.solar_savings_calculator, name='solar_savings_calculator'),
+    path('system-sizing/', views.solar_system_sizing_calculator, name='solar_system_sizing_calculator'),
 ]
